7679d9c47830f94f98f568bea4856506485fb928
Miguel Camilo <miguelcamilo48@gmail.com>
2023-02-16 10:18:28 -0500

    added the done patch request

--- functions/src/taskRequest.js
+++ functions/src/taskRequest.js
@@ -1,7 +1,7 @@
 import dbConnect from "./dbConnect.js"
 import { FieldValue } from "firebase-admin/firestore"
 
-export  function getAllTasks(req, res) {
+export   function getAllTasks(req, res) {
     const db =  dbConnect()
     // sortBy using FieldValue
     db.collection('tasks').orderBy('createdAt', 'desc').get()
@@ -12,10 +12,10 @@ export  function getAllTasks(req, res) {
         .catch(err => res.status(500).json({ error: err.message}))
 }
 
-export async function addTask (req, res) {
+export  function addTask (req, res) {
     const { tasks } = req.body
     const newTask = {tasks, createdAt: FieldValue.serverTimestamp()}
-    const db = await dbConnect() 
+    const db =  dbConnect() 
     db.collection('tasks').add(newTask)
 
     // here we are sending the newTask back to getAllTasks and it will send back the new task and all other tasks
@@ -23,9 +23,9 @@ export async function addTask (req, res) {
     .catch(err => res.status(500).send({ error: err.message }))
 }
 
-export async function deleteTask(req, res) {
+export  function deleteTask(req, res) {
     const { taskId } = req.params
-    const db = await dbConnect()
+    const db =  dbConnect()
 
     db.collection('tasks').doc(taskId).delete()
         .then(() => getAllTasks(req, res))
@@ -33,11 +33,11 @@ export async function deleteTask(req, res) {
 }
 
 
-export async function updateTask(req, res) {
+export  function updateTask(req, res) {
     const { done } = req.body
     const { taskId } = req.params
 
-    const db = await dbConnect()
+    const db =  dbConnect()
     db.collection('tasks').doc(taskId).update({ done })
         // here we are sending the updatedTasks back to getAllTasks and it will send back the update task and all other tasks
         .then(() => getAllTasks(req, res))
